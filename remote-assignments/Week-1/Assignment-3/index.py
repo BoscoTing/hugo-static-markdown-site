60dbe612531855d9cb21b7d53c6cb097805ac7f7
BoscoTing <110707173+BoscoTing@users.noreply.github.com>
2023-06-28 11:48:19 +0800

    Update index.py

--- remote-assignments/Week-1/Assignment-3/index.py
+++ remote-assignments/Week-1/Assignment-3/index.py
@@ -3,21 +3,21 @@
 #   <body>
 #     <h1>Assignment-3</h1>
 #     <p>
-      def find_max(numbers):
-          max_number = 0
-          for i in range(len(numbers)):
-              if numbers[i] > max_number:
-                  max_number = numbers[i]
-          return max_number
-      
-      def find_position(numbers, target):
-          list_position = []
-          for i in range(len(numbers)):
-              if numbers[i] == target:
-                  list_position.append(i)
-          if not list_position:
-              list_position.append(-1)
-          return list_position[0]
+def find_max(numbers):
+    max_number = 0
+    for i in range(len(numbers)):
+        if numbers[i] > max_number:
+            max_number = numbers[i]
+    return max_number
+
+def find_position(numbers, target):
+    list_position = []
+    for i in range(len(numbers)):
+        if numbers[i] == target:
+            list_position.append(i)
+    if not list_position:
+        list_position.append(-1)
+    return list_position[0]
 #     </p>
 #   <body>
 # </html>
