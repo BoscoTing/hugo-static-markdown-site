a1bae5a1ca6b11984610923b91118da0db3e78b2
Hao Ting <boscoting0522@gmail.com>
2023-07-01 19:43:04 +0800

    create .py for assignment

---
+++ remote-assignments/Week-1/Assignment-3/Assignment-3.py
@@ -0,0 +1,22 @@
+def find_max(numbers):
+  max_number = 0
+  for i in numbers:
+      if i > max_number:
+          max_number = i
+  return max_number
+
+def find_position(numbers, 
+                  target):
+    success, fail, t = 0, 0, 0
+    for num in numbers:
+        if num == target:
+            success += 1 
+            break
+        if num != target:
+            fail += 1
+        t += 1
+    if t == fail:
+        result = -1
+    if success >= 1:
+        result = t
+    return result 
