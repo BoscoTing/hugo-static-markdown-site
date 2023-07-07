5fbd3a10b3433768254a67e20d00e4e0cb24d774
Hao Ting <boscoting0522@gmail.com>
2023-07-07 11:46:26 +0800

    return -1 when not find target in numbers

--- remote-assignments/Week-1/Assignment-4/assignment_4.py
+++ remote-assignments/Week-1/Assignment-4/assignment_4.py
@@ -4,16 +4,11 @@ def binary_search_position(numbers,
         left = 0
         right = len(numbers)
         while left <= right:
-            mid = int((left+right) / 2)
+            mid = (left+right) // 2
             if numbers[mid] < target: 
                 left += 1
             elif numbers[mid] > target:
                 right -= 1
             else:
                 return mid
-    else: 
-        return -1
-
-print(binary_search_position([1, 2, 5, 6, 7], 1)) # should print 0 
-print(binary_search_position([1, 2, 5, 6, 7], 6)) # should print 3
-print(binary_search_position([1, 2, 5, 6, 7], 9))
+    return -1
