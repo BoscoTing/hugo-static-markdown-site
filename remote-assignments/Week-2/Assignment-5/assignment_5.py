079ec6bec2fbf2d6e918fcb3952769b224b218fc
BoscoTing <110707173+BoscoTing@users.noreply.github.com>
2023-07-07 15:42:50 +0800

    Update assignment_5.py

--- remote-assignments/Week-2/Assignment-5/assignment_5.py
+++ remote-assignments/Week-2/Assignment-5/assignment_5.py
@@ -12,32 +12,3 @@ def binary_search_first(numbers,
             return mid
 
     return -1
-
-# print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 2)) # should print 1 (the index of the target number ‘2’)
-# print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 5)) # should print 2 (the index of the ‘first’ target number ‘5’ shows up)
-# print(binary_search_first([1, 2, 5, 5, 5, 6, 7], 3)) # should print 2 (since it can’t find number 3, return the index of ‘the smallest number larger than 3', that is, the index of the ‘first’ number 5)
-
-def bisect_left(a, x, lo=0, hi=None, *, key=None):
-    if lo < 0:
-        raise ValueError('lo must be non-negative')
-    if hi is None:
-        hi = len(a)
-    # Note, the comparison uses "<" to match the
-    # __lt__() logic in list.sort() and in heapq.
-    if key is None:
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if a[mid] < x:
-                lo = mid + 1
-            else:
-                hi = mid
-    else:
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if key(a[mid]) < x:
-                lo = mid + 1
-            else:
-                hi = mid
-    return lo
-
-print(bisect_left([1, 2, 5, 5, 5, 6, 7], 3)) # should print 2 (since it can’t find number 3, return the index of ‘the smallest number larger than 3', that is, the index of the ‘first’ number 5)
