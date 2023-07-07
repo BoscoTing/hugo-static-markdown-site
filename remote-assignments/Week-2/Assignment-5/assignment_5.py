ea93354b1a60dcd9e6d832aa3d44429606557e6a
BoscoTing <110707173+BoscoTing@users.noreply.github.com>
2023-07-07 15:43:05 +0800

    Update assignment_5.py

--- remote-assignments/Week-2/Assignment-5/assignment_5.py
+++ remote-assignments/Week-2/Assignment-5/assignment_5.py
@@ -1,14 +1,1 @@
-def binary_search_first(numbers, 
-                        target):
-    left = 0
-    right = len(numbers)
-    while left <= right:
-        mid = (left+right) // 2
-        if numbers[mid] < target: # 下界
-            left += 1
-        elif numbers[mid] > target: # 上界
-            right -= 1
-        else:
-            return mid
 
-    return -1
