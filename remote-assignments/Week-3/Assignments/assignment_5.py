8f8ad62f6437772ebdfc3c720382690d55ec5b90
Hao Ting <boscoting0522@gmail.com>
2023-07-16 21:26:43 +0800

    add assignment_5.py

---
+++ remote-assignments/Week-3/Assignments/assignment_5.py
@@ -0,0 +1,22 @@
+def twoSum(nums, target):
+    i = 0 
+    right = len(nums) 
+    for num in nums: 
+        diff = target - num 
+        left = i 
+        while left < right: 
+            mid = (left+right) // 2 
+            if nums[mid] < diff: 
+                left += 1
+            elif nums[mid] > diff: 
+                right -= 1 
+            elif nums[mid-1] == diff:
+                right -= 1
+            else: 
+                return [i, mid]
+        i += 1 
+    return "Can't find the answer." 
+
+# time-comlexity: 
+# O(nlog n) + O(n) + O(1)
+# Big O = nlog n
