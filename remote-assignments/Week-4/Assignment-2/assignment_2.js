fcc60daf4eb49397efc96da2f80001504d2d1998
Hao Ting <boscoting0522@gmail.com>
2023-07-20 15:04:32 +0800

    assignment-2

--- remote-assignments/Week-4/Assignment-2/assignment_2.js
+++ remote-assignments/Week-4/Assignment-2/assignment_2.js
@@ -1,23 +1,48 @@
-function ajax(src, callback){
-    // your code here
-    let xhr = new XMLHttpRequest();
-    var url = src;
-    xhr.open("GET", url, true)
-    xhr.onreadystatechange == function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+function ajax(src, callback) {
+    const httpRequest = new XMLHttpRequest();
+    const url = src
+
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+            callback(httpRequest.response);
+        } else {
+            // alert('not ready');
+            return false;
+        }
+    }
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+};
+
+function render(data) {
+    const dataJson = JSON.parse(data)
+    const dataDict = Object.values(dataJson) 
+    const cols = Object.keys(Object.values(dataDict)[0]) // name, price, description
+    const dataKeys = Object.keys(dataDict)
+
+    i = 1
+    for (let key of dataKeys) { // key = 0, 1, 2
+        let itemDict = dataDict[key]; 
+        var newLi = document.createElement("li")
+        var newContent = document.createTextNode(`Item${i}`); // i = 1, 2, 3
+        newLi.appendChild(newContent);
+        var currentLi = document.getElementById("li");
+        document.body.insertBefore(newLi, currentLi);
+        i += 1
+
+        for (let itemKey of cols) { //name, price, description
+            let itemValue = itemDict[itemKey] // pixel3, 1700, ...
+            var newDiv = document.createElement("ul");
+            var newContent = document.createTextNode(`${itemKey}: ${itemValue}`);
+            newDiv.appendChild(newContent);
+            var currentDiv = document.getElementById("ul");
+            document.body.insertBefore(newDiv, currentDiv);
         }
     }
 };
 
-function render(data){
-    // your code here.
-    // document.createElement() and appendChild() methods are preferred.
-        var newDiv = document.createElement('div')
-        var newContent = document.createTextNode('Textnode created.')
-        newDiv.appendChild(newContent)
-};
-
 ajax("https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products",
-    function(response) { render(response);
-}); // you should get product information in JSON format and render data in the page
+    function(response) { 
+        render(response);
+});
+
